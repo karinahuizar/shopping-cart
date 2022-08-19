60ee126a1fcb8920677ddd03d931d52a2faa798a
Karina Huizar <karinahuizar@gmail.com>
2022-08-19 15:19:55 -0500

    Added more imports to App.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import Badge from '@material-ui/core/Badge'
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper } from "./App.styles";
 
 const App = () => {
   return <div className="App">
